2c9f8011b7e5c4531d8ffab854435473cbb0a3b1
jlpmartinss <jm8244936@gmail.com>
2025-03-10 16:25:14 +0000

    feat: add .env file for Supabase key and update supabase.js to use environment variable

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,8 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const supabaseUrl = "https://jsynmbyzpmkhnokorbkd.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpzeW5tYnl6cG1raG5va29yYmtkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE0ODE4NTAsImV4cCI6MjA1NzA1Nzg1MH0.G0mB2NUPKZ9VHEhw6A-U7c4G0x54NQf4GD_GSE15cmo";
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
